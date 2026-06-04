6a2923a54a7d580f24d20ece0f5a4d285a47822a
U1T-HoQuocViet <129581995+HQvilet@users.noreply.github.com>
2026-06-05 03:42:25 +0700

    Resolve merge

--- app/src/main/java/com/example/cinemabookingapp/core/config/ApiConfig.java
+++ app/src/main/java/com/example/cinemabookingapp/core/config/ApiConfig.java
@@ -3,8 +3,8 @@ package com.example.cinemabookingapp.core.config;
 public class ApiConfig {
 
     public static final String BASE_URL =
-            "http://10.0.159.143:8080/api/v1/";
-//            "http://10.0.2.2:8080/api/v1/";
+//            "http://10.0.159.143:8080/api/v1/";
+            "http://10.0.2.2:8080/api/v1/";
 
     public static final int CONNECT_TIMEOUT = 15;
     public static final int READ_TIMEOUT = 30;
